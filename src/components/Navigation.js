21c0e42a14319ff5f5a256c0c09f28b63b61493e
Eldon Hernandez <eldonhernandez@gmail.com>
2023-09-05 15:52:08 -0400

    Initial commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,30 @@
 import Navbar from 'react-bootstrap/Navbar';
-
 import logo from '../logo.png';
+
+// Function to shorten account
+const shortenAccount = (account, chars = 3) => {
+  if (account) {
+    return `${account.substring(0, chars)}...${account.substring(42 - chars)}`;
+  }
+  // Return a default value if account is not defined
+  return "Account Not Available";
+};
 
 const Navigation = ({ account }) => {
   return (
+    // Navigation Bar
     <Navbar className='my-3'>
       <img
         alt="logo"
         src={logo}
         width="40"
         height="40"
-        className="d-inline-block align-top mx-3"
+        className="d-inline-block align-top mx-0"
       />
-      <Navbar.Brand href="#">Dapp University Template</Navbar.Brand>
+      <Navbar.Brand href="#">Kalina Marketspace</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
-          {account}
+          {shortenAccount(account)}
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
