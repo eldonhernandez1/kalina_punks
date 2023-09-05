db38efedc8ba49168beca62ce601d17d6ea6300a
Eldon Hernandez <eldonhernandez@gmail.com>
2023-09-05 18:38:08 -0400

    Adding new files and updating code

---
+++ test/NFT.js
@@ -0,0 +1,40 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+describe('NFT', () => {
+  const NAME = 'Kalina Punks'
+  const SYMBOL = 'KP'
+  const COST = ether(10)
+  const MAX_SUPPLY = 25
+
+  let nft
+
+  describe('Deployment', () => {
+    beforeEach(async () => {
+      const NFT = await ethers.getContractFactory('NFT')
+      nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY)
+    })
+
+      it('has correct name', async () => {
+      expect(await nft.name()).to.equal(NAME)
+    })
+
+    it('has correct symbol', async () => {
+      expect(await nft.symbol()).to.equal(SYMBOL)
+    })
+
+    it('returns the cost to mint', async () => {
+      expect(await nft.cost()).to.equal(COST)
+    })
+
+    it('returns the maximum total supply', async () => {
+      expect(await nft.maxSupply()).to.equal(MAX_SUPPLY)
+    })
+  })
+})
